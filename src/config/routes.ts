074416debf7cd2829fba061f0028b9d4d7727570
YoshimasaIwano <44yoshimasa03@gmail.com>
2023-10-28 01:27:33 -0700

    init commit

---
+++ src/config/routes.ts
@@ -0,0 +1,11 @@
+const routes = {
+  admin: '/admin',
+  deploy: '/deploy',
+  initialize: '/initialize',
+  whitelist: '/whitelist',
+  add: '/add',
+  settings: '/settings',
+  authorize: '/authorize',
+};
+
+export default routes;
