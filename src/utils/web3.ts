bd915df10ee37a3c2ee6ed32a9a0762975f107e5
YoshimasaIwano <44yoshimasa03@gmail.com>
2023-10-29 11:03:07 -0700

    mistake

--- src/utils/web3.ts
+++ src/utils/web3.ts
@@ -9,7 +9,7 @@ if (typeof window !== 'undefined' && typeof (window as any).ethereum !== 'undefi
     // We're on the server or the user is not running metamask
     // Use the Sepolia endpoint provided by your provider. In this case, I'll use Infura as an example:
     const provider = new Web3.providers.HttpProvider(
-        'https://sepolia.infura.io/v3/69c296b88e394df1ad00a6170beb6cb0'
+      "https://eth-sepolia.g.alchemy.com/v2/x5TCqWvuugWM7--LT9YiC9IBMeUlQYvo"
     );
     web3 = new Web3(provider);
 }
